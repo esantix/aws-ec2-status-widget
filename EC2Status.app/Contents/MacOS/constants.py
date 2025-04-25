2748bb6c1da9ef532d43f44eaa468750e95b59f1
esantix <santiago93echevarria@gmail.com>
2025-04-25 00:28:28 -0300

    more verbose menus

--- EC2Status.app/Contents/MacOS/constants.py
+++ EC2Status.app/Contents/MacOS/constants.py
@@ -14,17 +14,16 @@ APP_STATE_ICON = {
     "on": "img/green.png", 
     "off": "img/gray.png" 
     }
-
 # Menu texts
 REFRESH_BUTTON_TEXT = "Refresh"
-OPEN_CONSOLE_BUTTON_TEXT = "Open console"
-SETTINGS_BUTTON_TEXT = "Open settings"
+OPEN_CONSOLE_BUTTON_TEXT = "Open AWS Console"
+SETTINGS_BUTTON_TEXT = "Settings..."
 NO_DATA_TEXT = "No data to display"
 # Sub menu texts
-START_BUTTON_TEXT = "✓  Start"
-STOP_BUTTON_TEXT = "⊖  Stop"
-COPY_INSTANCE_DATA_BUTTON_TEXT = "Copy EC2 data"
-VIEW_ON_CONSOLE_BUTTON_TEXT = "View on console"
+START_BUTTON_TEXT = "⏻  Start instance"
+STOP_BUTTON_TEXT = "⏼  Stop instance"
+COPY_INSTANCE_DATA_BUTTON_TEXT = "Copy data to clipboard"
+VIEW_ON_CONSOLE_BUTTON_TEXT = "View on Console"
 
 # Notification messages
 NOTIFICATION_TITLE = "EC2 Status"
